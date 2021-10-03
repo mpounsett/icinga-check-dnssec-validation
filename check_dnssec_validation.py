ca17c4cfb5800d9ed42a3170947aa82743a14529
Matthew Pounsett <matt@conundrum.com>
2021-10-03 15:52:33 -0400

    restructured to be loaded as module

--- check_dnssec_validation.py
+++ check_dnssec_validation.py
@@ -8,12 +8,14 @@ Licensed under the terms of the GNU GPL version 3 or higher
 """
 # Import the classes needed
 import argparse
-from sys import exit
+from sys import exit, argv
 try:
     import unbound
 except ImportError:
     print("Please install the python-bindings for unbound")
     exit(1)
+
+__version__ = '1.0.0'
 
 # Define and initialize global variables
 exit_ok = 0
@@ -22,16 +24,22 @@ exit_crit = 2
 exit_err = 3
 msg = ""
 
-parser = argparse.ArgumentParser(
-    description=(
-        "This script tests the zone for validation failures by looking up "
-        "the SOA record. This script is meant to be invoked by nagios/icinga."
+
+def parse_args(args=None):
+    args = args or argv[1:]
+
+    parser = argparse.ArgumentParser(
+        description=(
+            "This script tests the zone for validation failures by looking "
+            "up the SOA record. This script is meant to be invoked by "
+            "nagios/icinga."
+        )
     )
-)
-parser.add_argument("--zone", "-z", action="store", metavar='ZONE',
-                    help="The zone to be checked")
-parser.add_argument("--insecure-is-ok", action="store_true",
-                    dest="insecureOK")
+    parser.add_argument("--zone", "-z", action="store", metavar='ZONE',
+                        help="The zone to be checked")
+    parser.add_argument("--insecure-is-ok", action="store_true",
+                        dest="insecureOK")
+    return parser.parse_args(args)
 
 
 def quit(state):
@@ -47,32 +55,36 @@ def addToMsg(newString):
         msg += "%s" % newString
 
 
-# Script starts here.....
-args = parser.parse_args()
+def main():
+    args = parse_args()
 
-ctx = unbound.ub_ctx()
+    ctx = unbound.ub_ctx()
 
-# This key-file should be created once using unbound-anchor(8)
-ctx.set_option('auto-trust-anchor-file:', '/var/lib/icinga2/root.key')
+    # This key-file should be created once using unbound-anchor(8)
+    ctx.set_option('auto-trust-anchor-file:', '/var/lib/icinga2/root.key')
 
-status, result = ctx.resolve(args.zone, rrtype=unbound.RR_TYPE_SOA)
+    status, result = ctx.resolve(args.zone, rrtype=unbound.RR_TYPE_SOA)
 
-if status != 0:
-    addToMsg("UNKNOWN: resolver failure.")
-    quit(exit_err)
+    if status != 0:
+        addToMsg("UNKNOWN: resolver failure.")
+        quit(exit_err)
 
-if result.secure:
-    addToMsg("OK %s: the chain of trust is valid." % args.zone)
-    quit(exit_ok)
+    if result.secure:
+        addToMsg("OK %s: the chain of trust is valid." % args.zone)
+        quit(exit_ok)
 
-if result.bogus:
-    addToMsg("CRITICAL %s: %s" % (args.zone, result.why_bogus))
-    quit(exit_crit)
+    if result.bogus:
+        addToMsg("CRITICAL %s: %s" % (args.zone, result.why_bogus))
+        quit(exit_crit)
 
-# The result is insecure
-if args.insecureOK:
-    addToMsg("OK %s: is insecure." % args.zone)
-    quit(exit_ok)
+    # The result is insecure
+    if args.insecureOK:
+        addToMsg("OK %s: is insecure." % args.zone)
+        quit(exit_ok)
 
-addToMsg("WARNING %s: Result not secure (but not bogus)" % args.zone)
-quit(exit_warn)
+    addToMsg("WARNING %s: Result not secure (but not bogus)" % args.zone)
+    quit(exit_warn)
+
+
+if __name__ == '__main__':
+    main()
